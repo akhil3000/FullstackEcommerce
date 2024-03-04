097782bf23b9ea65f4bf7bca3b28f46d246e3920
AkhilPolisetty <akhilpolisetty456@gmail.com>
2024-03-04 19:48:57 +0530

    Pushing Backend code

---
+++ db.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+
+const connectDB=async()=>{
+   
+    const MONGO_URI="mongodb+srv://Akhil:Akhil1234@cluster0.ghgsmj9.mongodb.net/FullStackEcommerce?retryWrites=true&w=majority&appName=Cluster0"
+    try{
+       const conn=await mongoose.connect(MONGO_URI);
+       console.log(`MongoDB Connected:${conn.connection.host}`) 
+
+    }catch(error){
+      console.log(`Error:${error.message}`);
+      process.exit(1);
+    }
+
+}
+
+export default connectDB;
